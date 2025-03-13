a072c4c100bb1d8a91bdeac699804076de720003
deshan__maduranga <deshan.webmotech@gmail.com>
2025-03-13 12:59:19 +0530

    fix :fix the cors filter issu

--- src/main/java/lk/ijse/megacitycab/filter/CORSFilter.java
+++ src/main/java/lk/ijse/megacitycab/filter/CORSFilter.java
@@ -24,7 +24,11 @@ public class CORSFilter extends HttpFilter {
             res.setHeader("Access-Control-Expose-Headers", "Content-Type");
             res.setHeader("Access-Control-Allow-Credentials", "true");
         }
-
+        // Handle preflight request
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
         chain.doFilter(req, res);
     }
 }
