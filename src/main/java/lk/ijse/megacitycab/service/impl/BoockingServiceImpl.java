6f647bb92f021ada09cd0f5c24c816c0f600a699
deshan__maduranga <deshan.webmotech@gmail.com>
2025-03-13 19:49:18 +0530

    updt:update and fix the resolve the booking service impl

--- src/main/java/lk/ijse/megacitycab/service/impl/BoockingServiceImpl.java
+++ src/main/java/lk/ijse/megacitycab/service/impl/BoockingServiceImpl.java
@@ -11,5 +11,30 @@ import java.util.List;
 
 public class BoockingServiceImpl implements BoockingService {
 
+    private BoockingRepostory boockingRepostory = new BoockingRepostoryImpl();
+    private Map map = new Map();
+    @Override
+    public void saveBoocking(BoockingDTO boocking) throws IOException {
+        boockingRepostory.saveBoocking(map.toBoocking(boocking));
+    }
 
+    @Override
+    public List<BoockingDTO> getAllBoocking() throws IOException {
+        return map.toBoockingDtoList(boockingRepostory.getAllBoocking());
+    }
+
+    @Override
+    public void updateBoocking(BoockingDTO boocking) throws IOException {
+        boockingRepostory.updateBoocking(map.toBoocking(boocking));
+    }
+
+    @Override
+    public void deleteBoocking(String booking_id) throws IOException {
+        boockingRepostory.deleteBoocking(booking_id);
+    }
+
+    @Override
+    public BoockingDTO findBoocking(String booking_id) throws IOException {
+        return map.toBoockingDto(boockingRepostory.findBoocking(booking_id));
+    }
 }
